c988e1e033ed33c93dbea5857fe485ba163841c7
asphrix <yassinarkn5@gmail.com>
2024-09-17 21:04:00 +0300

    D: Added player movement

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
               return
-            
-        
-        
+     
         
         screen.fill("black")
 
