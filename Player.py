e566e030409055849267307ecad32cd2ef4e33b9
asphrix <yassinarkn5@gmail.com>
2024-09-18 22:58:25 +0300

    F: added shooting

--- Player.py
+++ Player.py
@@ -1,5 +1,6 @@
 from circleshape import *
 from constants import *
+from shot import * 
 class Player(CircleShape):
 
     def __init__(self, x, y, radius):
@@ -32,7 +33,14 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot(dt)
     
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+    
+    def shoot(self, dt):
+        shot = Shot(self.position.x, self.position.y)
+        vector = pygame.Vector2(0, 1).rotate(self.rotation)
+        shot.velocity = vector * PLAYER_SHOT_SPEED 
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from Player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -13,9 +14,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 10)
     meteors = AsteroidField()
 
